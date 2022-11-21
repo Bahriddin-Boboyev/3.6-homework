79fdfee2d68e157bba6a5eeb6a95e73b863c7ce9
scoderr <BahriddinBoboyev997@gmail.com>
2022-11-21 21:23:48 +0500

    3.6-homework

---
+++ main.js
@@ -0,0 +1,24 @@
+const car = {
+carName: "BMW",
+carModel: "BMW X8",
+carManufacturer: "BMW AG",
+carAssembly: "Germnay, USA",
+
+characteristic:{
+  carEngine: "750 HP",
+  EngineDisplacement: "2993",
+  wheelbase: "3105 mm",
+  maxSpeed: "850hp",
+  gearBox: "8-Speed Automatic",
+  fuelType: "Diesel",
+  fuelTankCapacity: "80.0",
+  turboCharger: "Twin"
+},
+
+price:{
+  dollorPrice: "$130,800",
+  euroPrice: "US$100,000",
+  sumPrice: "1148828993,96 UZS"
+}
+
+}
